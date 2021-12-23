8952134f68640b1d0e9b5774199ce175cdb3dc92
Alex218 <54863389+Alex218@users.noreply.github.com>
2021-12-23 23:02:16 +0400

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -1,13 +1,19 @@
-# Даны координаты трех точек (xa; ya) (xb; yb) (xc; yc),
-# точки соединены отрезками AB, BC и AC. Найдите отрезок с минимальной длинной.
-# Если отрезков с минимальной длиной несколько - вывести любой
-
-# При решении задачи необходимо использовать функцию расстояния между двумя точками.
-
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+    return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
 
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+def shortest_segments(a, b, c):
+    segments = {"AB": distance(*a, *b), "BC": distance(*b, *c), "AC": distance(*a, *c)}
+    min_length = segments["AB"]
+    for key in segments:
+        if segments[key] <= min_length:
+            shortest = key
+            min_length = segments[key]
+    return shortest
+
+
+a = 3, 2
+b = 1, -3
+c = 4, 10
+
+print("Самый короткий отрезок:", shortest_segments(a, b, c))
